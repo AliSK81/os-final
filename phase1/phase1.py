2c29313ff7d93f0a930dc4e97ae997a9d82d37c2
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-01-20 21:02:04 +0330

    use custom locker for phase1

--- phase1/phase1.py
+++ phase1/phase1.py
@@ -1,49 +1,59 @@
-from multiprocessing import Process, Value, Array, Queue
+from multiprocessing import Process, Value
 from time import sleep
 
+from locker import Locker
 
-def add(num, value):
+
+def add(num, value, locker):
     tmp = 0
     while True:
+        locker.lock(0)
         print('add')
         num.value += value
         tmp = num.value
         sleep(1)
         if tmp != num.value:
             print("Process conflict")
+        locker.unlock()
 
 
-def sub(num, value):
+def sub(num, value, locker):
     tmp = 0
     while True:
+        locker.lock(1)
         print('sub')
         num.value -= value
         tmp = num.value
         sleep(1.5)
         if tmp != num.value:
             print("Process conflict")
+        locker.unlock()
 
 
-def mul(num, value):
+def mul(num, value, locker):
     tmp = 0
     while True:
+        locker.lock(2)
         print('mul')
         num.value *= value
         tmp = num.value
         sleep(2)
         if tmp != num.value:
             print("Process conflict")
+        locker.unlock()
 
 
-def div(num, value):
+def div(num, value, locker):
     tmp = 0
     while True:
+        locker.lock(3)
         print('div')
         num.value /= value
         tmp = num.value
         sleep(3)
         if tmp != num.value:
             print("Process conflict")
+        locker.unlock()
 
 
 def Show(num):
@@ -54,12 +64,13 @@ def Show(num):
 
 if __name__ == '__main__':
     num = Value('d', 0.0)
-    arr = Array('i', range(2))
-    q = Queue()
-    p1 = Process(target=add, args=(num, 10))
-    p2 = Process(target=sub, args=(num, 5))
-    p3 = Process(target=mul, args=(num, 2))
-    p4 = Process(target=div, args=(num, 4))
+
+    locker = Locker(4)
+
+    p1 = Process(target=add, args=(num, 10, locker))
+    p2 = Process(target=sub, args=(num, 5, locker))
+    p3 = Process(target=mul, args=(num, 2, locker))
+    p4 = Process(target=div, args=(num, 4, locker))
 
     show = Process(target=Show, args=(num,))
     show.start()
