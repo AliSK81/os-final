06d5d775ca9b1c74bce65b6803c75a2569185839
Mohammad_Es <mohammad.es0281@gmail.com>
2023-01-21 14:29:06 +0330

    add locker to right and left sides

--- phase2/RightSide.py
+++ phase2/RightSide.py
@@ -1,24 +1,35 @@
+from multiprocessing import Queue, Value
 from time import sleep
 
 from Car import Car
+from Peterson import Locker
 
 
-def producer(queue, id, ):
+def producer(queue: Queue, id: Value, locker: Locker):
     print('Producer: Running', flush=True)
     while True:
+        if queue.full():
+            continue
+        locker.lock(1)
         value = Car(id.value)
         id.value += 1
+        locker.unlock(1)
         sleep(0.5)
         queue.put(value)
 
 
-def consumer(queue, street):
+def consumer(queue: Queue, street: Value, locker: Locker):
     print('Consumer: Running', flush=True)
     while True:
+        if queue.empty():
+            continue
+        locker.lock(1)
         item = queue.get()
         street.value = item.id
-        print('car id: ', item.id, 'sleep: ', item.time)
+        print('RC', 'car id: ', item.id, 'sleep: ', item.time)
         temp = street.value
         sleep(item.time)
         if temp != street.value:
             print('Process conflict!')
+        street.value = 0
+        locker.unlock(1)
--- phase2/phase2.py
+++ phase2/phase2.py
@@ -2,6 +2,7 @@ from multiprocessing import Process, Queue, Value
 
 import LeftSide
 import RightSide
+from Peterson import Locker
 
 
 def main():
@@ -11,13 +12,16 @@ def main():
     car_list1 = Queue(maxsize=10)
     car_list2 = Queue(maxsize=10)
 
-    prod1 = Process(target=RightSide.producer, args=(car_list1, id))
-    cons1 = Process(target=RightSide.consumer, args=(car_list1, street))
+    producer_locker = Locker()
+    consume_locker = Locker()
+
+    prod1 = Process(target=RightSide.producer, args=(car_list1, id, producer_locker))
+    cons1 = Process(target=RightSide.consumer, args=(car_list1, street, consume_locker))
     prod1.start()
     cons1.start()
 
-    prod2 = Process(target=LeftSide.producer, args=(car_list2, id))
-    cons2 = Process(target=LeftSide.consumer, args=(car_list2, street))
+    prod2 = Process(target=LeftSide.producer, args=(car_list2, id, producer_locker))
+    cons2 = Process(target=LeftSide.consumer, args=(car_list2, street, consume_locker))
     prod2.start()
     cons2.start()
 
